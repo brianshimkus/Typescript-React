09e0b291e7367dd2c36bde6f7148a1ae9d4a8b6b
Brian Shimkus <brianshimkus1@gmail.com>
2025-05-12 18:06:23 -0500

    Defined props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Person } from './Person'
+
 function App() {
-	return <></>
+	return (
+		<>
+			<Person name={'Jeff'} age={33} isMarried={false} />
+			<Person name={'Colin'} age={36} isMarried={true} />
+		</>
+	)
 }
 
 export default App
