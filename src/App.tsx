8ea11b8f5a09715da180345fb6fa3236a359aa89
Brian Shimkus <brianshimkus1@gmail.com>
2025-05-13 11:20:35 -0500

    User Context Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { User } from './user'
+import { UserProvider } from './UserContextProvider'
 
 function App() {
 	return (
-		<>
+		<UserProvider>
 			<User name={'Jeff'} age={33} isMarried={false} />
 			<User name={'Colin'} age={36} isMarried={true} />
-		</>
+		</UserProvider>
 	)
 }
 
--- src/User.tsx
+++ src/User.tsx
@@ -1,27 +1,13 @@
-import { useState } from 'react'
-
-export interface User {
-	name: string
-	age: number
-	isMarried: boolean
-}
+// import { useContext } from 'react'
+// import { UserContext } from './UserContextProvider'
 
 export const User = (props: User) => {
-	const [userBio, setUserBio] = useState<string | null>('')
-
-	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setUserBio(e.target.value)
-	}
-
+	// const { users, updateUser, deleteUser } = useContext(UserContext)
 	return (
 		<div>
 			<p>Name: {props.name}</p>
 			<p>Age: {props.age}</p>
 			<p>This user: {props.isMarried ? 'is married' : 'is single'}</p>
-			<p>
-				{props.name} Bio: {!userBio ? 'No Bio Available' : userBio}
-			</p>
-			<input onChange={handleChange} />
 		</div>
 	)
 }
