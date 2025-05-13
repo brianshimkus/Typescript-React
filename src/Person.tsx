3a9d28ecfcc4b761f6d724f26578255af4066123
Brian Shimkus <brianshimkus1@gmail.com>
2025-05-13 11:00:45 -0500

    Form events

--- src/Person.tsx
+++ src/Person.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 export interface Person {
 	name: string
 	age: number
@@ -5,11 +7,21 @@ export interface Person {
 }
 
 export const Person = (props: Person) => {
+	const [personBio, setPersonBio] = useState<string | null>('')
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setPersonBio(e.target.value)
+	}
+
 	return (
 		<div>
 			<p>Name: {props.name}</p>
 			<p>Age: {props.age}</p>
 			<p>This person: {props.isMarried ? 'is married' : 'is single'}</p>
+			<p>
+				{props.name} Bio: {!personBio ? 'No Bio Available' : personBio}
+			</p>
+			<input onChange={handleChange} />
 		</div>
 	)
 }
